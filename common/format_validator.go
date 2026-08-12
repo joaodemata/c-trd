54d2a51d8629aaf010e87ac1c6b3eca68d700ea8
jmata <joao28juan@gmail.com>
2026-08-12 15:49:04 +0100

    Crear primera version de un format validate generico y crear archivos para nuevo modelo de historico de cambios en oportunidades y api de finhubb

---
+++ common/format_validator.go
@@ -0,0 +1,60 @@
+package common
+
+import (
+	"encoding/json" // Importamos el paquete de errores estándar
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+)
+
+// Inicializamos el validador a nivel global (es thread-safe y optimiza el rendimiento)
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
+}
+
+// Validador de si hubo errores y que tipo fue para devolverlo
+func FormatValidateHandler(w http.ResponseWriter, r *http.Request, payloadFormat any) {
+	err := json.NewDecoder(r.Body).Decode(payloadFormat)
+
+	if err != nil {
+		// Verificar si el error es de tipo de dato erroneo
+		var typeError *json.UnmarshalTypeError
+		
+		if errors.As(err, &typeError) {
+			// Mismatch data type
+			message := fmt.Sprintf("Error de tipo en el campo '%s': se esperaba %s pero se recibió un %s", 
+				typeError.Field, typeError.Type.String(), typeError.Value)
+			
+			http.Error(w, message, http.StatusBadRequest)
+			return
+		}
+
+		// No es formato JSON
+		http.Error(w, "El JSON está mal formateado o es inválido estructuralmente", http.StatusBadRequest)
+		return
+	}
+	// Validammos la estructura del JSON en base a las reglas enviadas
+	err = validate.Struct(payloadFormat)
+
+	if err != nil {
+		// Si la validación falla, extraemos los errores para que el cliente sepa qué pasó
+		var errors []string
+		for _, err := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("El campo '%s' falló en la regla: '%s'", err.Field(), err.Tag())
+			errors = append(errors, errorMessage)
+		}
+
+		// Devolvemos un HTTP 400 (Bad Request) con el array de errores
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error":    "Datos de entrada inválidos",
+			"detalles": errors,
+		})
+		return
+	}
+}
---
+++ services/finhubb_service.go
@@ -0,0 +1,3 @@
+package services
+
+// Apis de finnhub para activar o desactivar condiciones
